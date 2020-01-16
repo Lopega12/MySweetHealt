f4db400c835027a441c76a2dfa22c1e044031ff2
Lorenzo <lorenzoperezga@gmail.com>
2020-01-16 19:46:37 +0100

    Primer Commit con WorkSpace
    Commit Inicial

---
+++ BienestarDigital/BienestarDigital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BienestarDigital
+//
+//  Created by Loren on 16/01/2020.
+//  Copyright © 2020 Loren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
