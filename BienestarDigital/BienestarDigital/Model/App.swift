8d2c7486ed0a4581adb6abebb580210f03e66106
Lorenzo <lorenzoperezga@gmail.com>
2020-01-25 18:11:07 +0100

    Segundo Commit con pod del menu funcionando

---
+++ BienestarDigital/BienestarDigital/Model/App.swift
@@ -0,0 +1,9 @@
+//
+//  App.swift
+//  BienestarDigital
+//
+//  Created by Loren on 25/01/2020.
+//  Copyright © 2020 Loren. All rights reserved.
+//
+
+import Foundation
---
+++ BienestarDigital/BienestarDigital/ProfileController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileController.swift
+//  BienestarDigital
+//
+//  Created by Loren on 18/01/2020.
+//  Copyright © 2020 Loren. All rights reserved.
+//
+
+import Foundation
